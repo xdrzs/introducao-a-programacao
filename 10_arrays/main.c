8e95c69f69aef853a8688ff8ae83ba5d90b7dc68
LuizTrMr <luizfelipetmoraes@hotmail.com>
2025-04-16 17:33:49 -0300

    Add versdão 2 do 10_arrays

--- 10_arrays/main.c
+++ 10_arrays/main.c
@@ -19,29 +19,24 @@ int main() {
 	array[2] = 25;
 	printf("array[2] = %d\n", array[2]);
 
+	// printf("%d\n", array[3]); // array bounds checking -> checagem de limite de arrays
+
 	printf("------\n");
 	for (int i = 0; i < 3; i += 1) {
 		printf("array[%d] = %d\n", i, array[i]);
 	}
 
-	// printf("array[3] = %d\n", array[3]); // array bound checking -> checagem de limites para arrays
-
-	float pesos_argentinos[4] = { 1100, 600, 40100, 580 };
+	// C89
+	printf("------\n");
+	float pesos_argentinos[] = {1100, 650, 2040.5, 11050, 700};
+	printf("%zu\n", sizeof(pesos_argentinos));
 	float soma = 0;
-	for (int i = 0; i < 4; i += 1) {
-		soma += pesos_argentinos[i];
-	}
+	for (int i = 0; i < sizeof(pesos_argentinos)/sizeof(pesos_argentinos[0]); i++) soma += pesos_argentinos[i];
 	printf("soma = %f\n", soma);
 
-	printf("----\n");
-	// C89
-	float reais[] = {15.50, 200, 100, 48.9, 77.0};
-	printf("sizeof(reais) = %zu\n", sizeof(reais));
-	for (int i = 0; i < sizeof(reais)/sizeof(reais[0]); i++) printf("reais[%d] = %f\n", i, reais[i]);
-
-	char meu_nome[N] = {'L', 'u', 'i', 'z'};
+	char nome[N] = {'L', 'u', 'i', 'z'};
 	for (int i = 0; i < N; i++) {
-		printf("%c", meu_nome[i]);
+		printf("%c", nome[i]);
 	}
 	printf("\n");
 
