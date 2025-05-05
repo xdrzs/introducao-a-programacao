6f8a064ea8a98705367b9b116d0a080ae1ef9939
xdrzs <xdrzdm@gmail.com>
2025-05-05 15:01:19 -0300

    Add 05_hello_world

---
+++ 05_hello_world/programa.c
@@ -0,0 +1,14 @@
+// include -> incluir
+/*
+std: standard -> padrão
+i:   input -> entrada
+o:   output -> saída
+*/
+#include <stdio.h>
+
+// int: integer -> inteiro (ex: 0 10 2 76 -81 -10004)
+// main -> principal
+int main() {
+	printf("Hello World\n");
+	return 0; // return -> retornar
+}
